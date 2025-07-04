7161a0a79f6b30abde57300a831e8ca43f192971
gustavosrosa <g.dsantosrosa@gmail.com>
2025-07-04 13:53:21 -0300

    CURS: FEAT: CRUD inicial Node

--- index.js
+++ index.js
@@ -3,14 +3,58 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get("/curso/:ano", (req, res) => {
+app.use(express.json());
 
-    const nome = req.query.nome;
-    const ano = req.params.ano;
+const cursos = ["NodeJS", "Java", "JavaScript", "PHP", "Python"];
+
+/**
+ * Listagem de todos os cursos
+ */
+app.get("/cursos", (req, res) => {
+    res.json(cursos);
+});
+
+/**
+ * Listagem de um curso por ID
+ */
+app.get("/cursos/:index", (req, res) => {
+
+    const index = req.params.index;
 
     res.json({
-        OK: `Aprendendo ${nome} em ${ano}`
+        Curso: cursos[index]
     })
+})
+
+/**
+ * Adicionar curso
+ */
+app.post("/cursos", (req, res) => {
+    cursos.push(req.body.curso);
+    res.json(cursos);
+});
+
+/**
+ * Alterar curso
+ */
+app.put("/cursos/:index", (req, res) => {
+    const index = req.params.index;
+    const curso = req.body.curso;
+
+    cursos[index] = curso;
+
+    res.json(cursos);
+})
+
+/**
+ * Remover Curso
+ */
+app.delete("/cursos/:index", (req, res) => {
+    const index = req.params.index;
+
+    cursos.splice(index, 1);
+
+    res.json(cursos)
 })
 
 app.listen(PORT);
